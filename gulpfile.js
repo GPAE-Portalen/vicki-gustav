3e4ee5cbd95fd65bc0a02f899df047393493a8e4
Gustav Eklund <gustavpaeklund@gmail.com>
2021-09-25 15:11:38 +0200

    Test

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,22 @@ const markdownToJSON = require('gulp-markdown-to-json');
 const marked = require('marked');
 const jsoncombine = require('gulp-jsoncombine');
 const beautify = require('gulp-jsbeautifier');
+const fs = require('fs');
 
 // Clean JSON data
 gulp.task('cleanJson', async () => {
+    function createJsonData(dataJsonFileName) {
+        fs.writeFileSync(`./src/data/${dataJsonFileName}.json`, JSON.stringify({}))
+    }
+
     gulp.src("./content/json/**/*.json", { read: false })
         .pipe(clean())
+
+    gulp.src("./src/data/**/*.json", { read: false })
+        .pipe(clean())
+
+    createJsonData('blog');
+    createJsonData('recipes');
 });
 
 // Generate JSON data from markdown
@@ -19,24 +30,43 @@ marked.setOptions({
 gulp.task('generateJson', async () => {
     gulp.src('./content/markdown/**/*.md')
         .pipe(markdownToJSON(marked))
+        .pipe(beautify())
         .pipe(gulp.dest('./content/json'))
 });
 
 // Combine JSON data
 gulp.task('combineJson', async () => {
-    gulp.src("./content/json/blog/*.json")
-        .pipe(jsoncombine("blog.json", function (data, meta) {
-            return new Buffer(JSON.stringify(data));
-        }))
-        .pipe(beautify())
-        .pipe(gulp.dest("./src/data"));
+    function combine(contentJsonDirName, dataJsonFileName) {
+        const jsonContentSrc = `./content/json/${contentJsonDirName}/*.json`;
+        const dataJsonFile = `${dataJsonFileName}.json`;
+        const dataPath = './src/data';
 
-    gulp.src("./content/json/recipes/*.json")
-        .pipe(jsoncombine("recipes.json", function (data, meta) {
-            return new Buffer(JSON.stringify(data));
-        }))
-        .pipe(beautify())
-        .pipe(gulp.dest("./src/data"));
+        gulp.src(jsonContentSrc)
+            .pipe(jsoncombine(dataJsonFile, function (data, meta) {
+                return new Buffer.from(JSON.stringify(data));
+            }))
+            .pipe(beautify())
+            .pipe(gulp.dest(dataPath));
+    }
+
+    function handleSingularFile(dataJsonFileName) {
+        const jsonFile = `${dataJsonFileName}.json`;
+        const jsonContentSrc = `./content/json/${jsonFile}`;
+        const dataPath = './src/data';
+
+        gulp.src(jsonContentSrc)
+            .pipe(beautify())
+            .pipe(gulp.dest(dataPath));
+    }
+
+    combine('blog', 'blog');
+    combine('recipies', 'recipies');
+});
+
+// Clean JSON data
+gulp.task('cleanImages', async () => {
+    gulp.src("./public/assets/**/*", { read: false })
+        .pipe(clean())
 });
 
 // Copy images from assets folder to public folder
